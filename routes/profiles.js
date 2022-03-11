b1bb4df4f165791da2284c12308f9d2f7e64e17b
KarthikS737 <lit2021012@iiitl.ac.in>
2022-03-11 21:52:20 +0530

    hobbies

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,6 +2,11 @@ const data = require('../profiles.json')
 const express = require("express")
 const router = express.Router()
 
+router.get('/hobbies', (req, res) => { // returns profiles of all the participants
+    let responce = [... new Set(data.flatMap(e => e.hobbies))];  // [... new Set()] is used to filter out unique elements
+    res.send(responce)
+});
+
 router.get('/:roll_number', (req, res) => { // returns profile with the requested roll number
     const result = data.find(profile => profile.roll_number.toLowerCase() === req.params.roll_number.toLowerCase())
     if(result) res.send(result)
