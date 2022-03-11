51aafa69ddad627aadc98db57405b85ef98b5e71
KarthikS737 <lit2021012@iiitl.ac.in>
2022-03-11 22:14:52 +0530

    rolls

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,6 +2,10 @@ const data = require('../profiles.json')
 const express = require("express")
 const router = express.Router()
 
+router.get('/roll-numbers', (req, res) => { // returns profiles of all the participants
+    res.send([ ... new Set(data.map(e => e.roll_number))]);
+})
+
 router.get('/:roll_number', (req, res) => { // returns profile with the requested roll number
     const result = data.find(profile => profile.roll_number.toLowerCase() === req.params.roll_number.toLowerCase())
     if(result) res.send(result)
