685a3173e091beaf1d80886de3c15ec7661aacfc
Animesh Sahu <animeshsahu19@yahoo.com>
2022-03-11 08:50:28 +0530

    Add endpoint for listing all the possible hobbies

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,6 +2,13 @@ const data = require('../profiles.json')
 const express = require("express")
 const router = express.Router()
 
+router.get('/hobbies', (req, res) => { // returns a list of all the possible hobbies in the participants list
+    const unique = (element, index, self) => self.indexOf(element) === index;
+    const result = data.flatMap(e => e["hobbies"]).filter(unique);
+
+    res.send(result);
+})
+
 router.get('/:roll_number', (req, res) => { // returns profile with the requested roll number
     const result = data.find(profile => profile.roll_number.toLowerCase() === req.params.roll_number.toLowerCase())
     if(result) res.send(result)
