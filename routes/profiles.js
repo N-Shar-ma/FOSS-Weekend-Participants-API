7e2ef21af6752379f3aba279529bd2ae941bc3be
flyingdelta <flyingdelta@outlook.com>
2022-03-11 20:37:48 +0530

    All Hobbies

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,19 @@ const express = require("express")
 const router = express.Router()
 
 router.get('/:roll_number', (req, res) => { // returns profile with the requested roll number
-    const result = data.find(profile => profile.roll_number.toLowerCase() === req.params.roll_number.toLowerCase())
+    let rollNumber = req.params.roll_number;
+    let result;
+    if (rollNumber == "hobbies"){
+        result = new Set()
+        for (let p of data){
+            for (let h of p.hobbies){
+                result.add(h)
+            }
+        }
+        result = [...result]
+    } else {
+        result = data.find(profile => profile.roll_number.toLowerCase() === rollNumber.toLowerCase())
+    }
     if(result) res.send(result)
     else res.sendStatus(404)
 })
