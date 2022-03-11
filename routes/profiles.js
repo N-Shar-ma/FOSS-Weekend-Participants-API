3ebbccf6b7dac006751e55af0ffe7dd891c28438
Animesh Sahu <animeshsahu19@yahoo.com>
2022-03-11 08:36:09 +0530

    Fixes #3, adds detection of hobby GET parameter to return all participant data which matches.

--- routes/profiles.js
+++ routes/profiles.js
@@ -8,7 +8,14 @@ router.get('/:roll_number', (req, res) => { // returns profile with the requeste
     else res.sendStatus(404)
 })
 
-router.get('/', (req, res) => { // returns profiles of all the participants
+router.get('/', (req, res) => {
+    // If GET parameter hobby is present then return matching profiles
+    if (req.query.hobby != undefined) {
+        res.send(data.filter(e => e["hobbies"].includes(req.query.hobby)));
+        return;
+    }
+
+    // Or else returns profiles of all the participants
     res.send(data)
 })
 
